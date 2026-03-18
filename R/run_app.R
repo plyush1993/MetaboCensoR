98b6f390c7e549e9640bc60b97693786a6c486a3
Ivan Plyushchenko, PhD <plyush1993@bk.ru>
2026-03-18 22:58:33 +0200

    Add files via upload

--- R/run_app.R
+++ R/run_app.R
@@ -3,6 +3,7 @@
 #'
 #' @export
 run_metabocensor <- function(...) {
+  options(shiny.maxRequestSize = 5 * 1024^3)
   shiny::addResourcePath("www", system.file("www", package = "MetaboCensoR"))
   shiny::shinyApp(ui = app_ui(), server = app_server, ...)
 }
