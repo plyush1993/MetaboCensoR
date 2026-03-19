24cc57b156a3ebd6f9a26e414f19e91e7c762a95
Ivan Plyushchenko, PhD <plyush1993@bk.ru>
2026-03-19 22:05:42 +0200

    Add files via upload

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,25 @@
 #' Run the MetaboCensoR Application
 #' Online version: https://plyush1993.shinyapps.io/metabocensor/
 #'
+#' @importFrom crayon blue red cyan %+%
 #' @export
 run_metabocensor <- function(...) {
-  options(shiny.maxRequestSize = 5 * 1024^3)
+  cat("\n")
+  cat(crayon::cyan("             ╔════════════════════╗\n"))
+  cat(crayon::cyan("             ║ "), crayon::blue$bold('   Metabo' %+% red$bold('CensoR   ')), crayon::cyan(" ║\n"), sep = "")
+  cat(crayon::cyan("             ╚════════════════════╝\n"))
+  cat("\n")
+  cat(crayon::cyan$bold("Shiny App for filtering redundant features in LC-MS\n"))
+  cat("\n")
+
+  flush.console()
+
+  old_opts <- options(shiny.maxRequestSize = 5 * 1024^3)
+    on.exit({
+    options(old_opts)
+    gc()
+    }, add = TRUE)
+
   shiny::addResourcePath("www", system.file("www", package = "MetaboCensoR"))
   shiny::shinyApp(ui = app_ui(), server = app_server, ...)
 }
